030dc109753534ac7091bb9e47c6a7709318797c
osShoppy <osshoppy@gmail.com>
2013-01-13 11:39:06 +0100

    edited copyrights

--- project/admin/includes/footer.php
+++ project/admin/includes/footer.php
@@ -3,13 +3,12 @@
 	</td>
   </tr>
 </table>
-<table align="center" border="0" width="<?php  echo ADMIN_WIDTH?>" cellspacing="0" cellpadding="2">
+<table width="100%" cellspacing="0" cellpadding="0" class="indexFooter" align="center">
   <tr>
     <td align="center" class="smallText">
-		osShoppy Copyright &copy; 2012 <a href="http://www.hilvy.nl" target="_blank">hilvy.nl</a><br />
+		osShoppy Copyright &copy; 2012 <a href="http://www.osShoppy.com" target="_blank">osShoppy</a><br />
 		Released under the <a href="http://www.fsf.org/licenses/gpl.txt" target="_blank">GNU General Public License</a><br />
-		Powered by <a href="http://www.oscommerce.com" target="_blank">osCommerce</a><br />
+		Powered by <a href="http://www.oscommerce.com" target="_blank">osCommerce</a>
     </td>
   </tr>
 </table>
- 
