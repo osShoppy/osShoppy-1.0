c251364172466c6f5a1c8c9bc226270a376485c1
osShoppy <osshoppy@gmail.com>
2013-01-15 17:31:00 +0100

    update header menu

--- project/admin/includes/header.php
+++ project/admin/includes/header.php
@@ -27,18 +27,15 @@ var url = 'http://<?php echo STORE_URL ?>/live_support/staff.php';
     }
   }
 ?>
-
 <link rel="stylesheet" type="text/css" href="templates/<?php echo ADMIN_TEMPLATE?>/tabs.css"/>
 <link rel="stylesheet" type="text/css" href="templates/<?php echo ADMIN_TEMPLATE?>/stylesheet.css"/>
 <link rel="stylesheet" type="text/css" href="templates/<?php echo ADMIN_TEMPLATE?>/ddsmoothmenu.css" />
 <link rel="shortcut icon" href="templates/<?php echo ADMIN_TEMPLATE?>/favicon.ico">
-
 <!--[if lte IE 7]>
 <style type="text/css">
 html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
 </style>
 <![endif]-->
-
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
 <script type="text/javascript" src="templates/<?php echo ADMIN_TEMPLATE?>/js/jquery-1.2.6.js"></script>
 <script type="text/javascript" src="templates/<?php echo ADMIN_TEMPLATE?>/js/ui.core.js"></script>
@@ -51,21 +48,16 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
 ***********************************************/
 </script>
 
-
-
 <table class="tableBody"  align="center"  border="0" width="<?php  echo ADMIN_WIDTH?>" height="800px" cellspacing="0" cellpadding="0">
   <tr>
     <td valign="top" >
-
-<table class="headerBody" border="0" width="100%" cellspacing="0" cellpadding="0">
-  <tr>
-    <td width="400" rowspan="2"><a   href="index.php"><img src="templates/<?php  echo ADMIN_TEMPLATE?>/images/store_logo.png" border="0" /></a>
- </td>
-   
-  </tr>
-  <tr>
-    <td width="400" align="right" valign="bottom"> <div style="padding-right:10px; padding-top:10px;"><table  width="400" height="86" border="0" cellpadding="0" cellspacing="0">
-	<tr>
+	  <table class="headerBody" border="0" width="100%" cellspacing="0" cellpadding="0">
+  	   <tr>
+   		 <td width="400" rowspan="2"><a   href="index.php"><img src="templates/<?php  echo ADMIN_TEMPLATE?>/images/store_logo.png" border="0" /></a></td>
+       </tr>
+       <tr>
+        <td width="400" align="right" valign="bottom"> <div style="padding-right:10px; padding-top:10px;"><table  width="400" height="86" border="0" cellpadding="0" cellspacing="0">
+	   <tr>
         <td width="79" height="45" align="center" valign="bottom"><?php echo '<a href="javascript:phpOnlinePopupWindowAdmin()">' . tep_image(DIR_WS_IMAGES . 'icons/header_icons/header_icon_livesupport.png', HEADER_LIVE_SUPPORT) . '</a>'; ?></td>
         <td width="79" height="45" align="center" valign="bottom">			<a href="<?php echo FORUM_URL; ?>" class="headerNavigation" target="_blank"><?php echo tep_image(DIR_WS_IMAGES . 'icons/header_icons/header_icon_forum.png', HEADER_FORUM); ?></a></td>
         <td width="79" align="center" valign="bottom"><?php echo '<a href="' . tep_href_link(FILENAME_ORDERS) . '">' . tep_image(DIR_WS_IMAGES . 'icons/header_icons/header_icon_orders.png', HEADER_ORDER) . '</a>'; ?></td>
@@ -76,18 +68,15 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
 		<td width="79" align="center" valign="bottom"><?php echo '<a href="' . tep_href_link(FILENAME_STATS_CREDITS) . '">' . tep_image(DIR_WS_IMAGES . 'icons/header_icons/header_icon_accountsaldo.png', HEADER_ACCOUNT_SALDO) . '</a>'; ?></td> 
 		<td width="79" align="center" valign="bottom"><?php echo '<a href="' . tep_href_link(FILENAME_MANUAL_INDEX) . '">' . tep_image(DIR_WS_IMAGES . 'icons/header_icons/header_icon_manual_index.png', HEADER_MANUAL_INDEX) . '</a>'; ?></td> 
 		<td width="79" align="center" valign="bottom"><?php echo '<a href="' . tep_catalog_href_link() . '" target="_blank" >' . tep_image(DIR_WS_IMAGES . 'icons/header_icons/header_icon_toshop.png', HEADER_TO_SHOP) . '</a>'; ?></td> 
-	</tr>
-	<tr>
+	   </tr>
+	   <tr>
 		<td colspan="7" class="date" align="right"><?php echo strftime(DATE_TIME_FORMAT_LONG); ?>&nbsp;&nbsp;&nbsp;</td>
 		<td colspan="3" class="text" align="center"><?php echo tep_draw_form('languages', 'index.php', '', 'get'); ?>
         <?php echo tep_draw_pull_down_menu('language', $languages_array, $languages_selected, 'onChange="this.form.submit();"'); ?>
         </form></td>
-	</tr>
-	
-</table>
-   </div>
-
-                      
+	   </tr>
+	  </table>
+     </div>
     </td>
     <td width="234" align="center" valign="middle" class="text">
     	<?php
@@ -125,7 +114,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
 </table>
 <div id="smoothmenu1" class="ddsmoothmenu">
 <ul>
-
 <li><div class="line"><a href="#"><?php echo BOX_HEADING_ADMIN_TOP ?></a></div>
   <ul>
   <?php   if (tep_admin_check_boxes('administrator_top.php') == true) {
@@ -134,7 +122,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_ADMIN_MID ?></a></div>
   <ul>
   <?php   if (tep_admin_check_boxes('administrator_mid.php') == true) {
@@ -143,7 +130,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_ADMIN_LOW ?></a></div>
   <ul>
   <?php   if (tep_admin_check_boxes('administrator_low.php') == true) {
@@ -152,7 +138,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a href="#"><?php echo BOX_HEADING_CATALOG ?></a></div>
   <ul>
   <?php if (tep_admin_check_boxes('catalog.php') == true) {
@@ -161,7 +146,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#"><?php echo BOX_HEADING_CUSTOMERS ?></a></div>
   <ul>
   <?php  if (tep_admin_check_boxes('customers.php') == true) {
@@ -170,7 +154,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#"><?php echo BOX_HEADING_TOOLS ?></a></div>
   <ul>
   <?php  if (tep_admin_check_boxes('tools.php') == true) {
@@ -178,7 +161,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   }?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_MODULES ?></a></div>
   <ul>
  <?php  if (tep_admin_check_boxes('modules.php') == true) {
@@ -187,7 +169,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_CHECKOUT ?></a></div>
   <ul>
  <?php  if (tep_admin_check_boxes('checkout.php') == true) {
@@ -196,7 +177,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_REPORTS ?></a></div>
   <ul>
   <?php if (tep_admin_check_boxes('reports.php') == true) {
@@ -204,7 +184,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   }?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_LOCATION_AND_TAXES ?></a></div>
   <ul>
   <?php if (tep_admin_check_boxes('taxes.php') == true) {
@@ -213,7 +192,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_LOCALIZATION ?></a></div>
   <ul>
   <?php if (tep_admin_check_boxes('localization.php') == true) {
@@ -222,7 +200,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 <li><div class="line"><a  href="#" ><?php echo BOX_HEADING_SUPPORT ?></a></div>
   <ul>
  <?php  if (tep_admin_check_boxes('support.php') == true) {
@@ -231,8 +208,6 @@ html .ddsmoothmenu{height: 1%;} /*Holly Hack for IE7 and below*/
   ?>
   </ul>
 </li>
-
 </ul>
 <br style="clear: left" />
 </div>
-
